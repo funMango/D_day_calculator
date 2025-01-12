6205298ff3735c2ca8eb29df9c77460ff9961a77
MinhoLee <bluemango608@gmail.com>
2025-01-12 15:17:40 +0900

    start

---
+++ D_day_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  D_day_calculatorApp.swift
+//  D_day_calculator
+//
+//  Created by 이민호 on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct D_day_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
