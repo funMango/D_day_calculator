752a5483b23f4e71a1d5e927def9e202223e219b
funMango <138420832+funMango@users.noreply.github.com>
2025-01-16 21:14:17 +0900

    feat: String에서 Date타입으로 변경기능 구현

--- D_day_calculator/Utilities/Convertor.swift
+++ D_day_calculator/Utilities/Convertor.swift
@@ -14,3 +14,15 @@ struct Convertor {
         return dateFormatter.string(from: date)
     }
 }
+
+enum DateFormat: String {
+    case USA = "MMM d, yyyy"
+}
+
+extension Date {
+    func formatted(_ format: String) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format
+        return dateFormatter.string(from: self)
+    }
+}
