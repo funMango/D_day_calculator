08a4a08668216cb6aaf9d29843c940c912f1f70a
funMango <138420832+funMango@users.noreply.github.com>
2025-02-25 19:02:48 +0900

    refactor: 지원 홈페이지 추가

--- D_day_calculator/Models/Url.swift
+++ D_day_calculator/Models/Url.swift
@@ -7,9 +7,10 @@
 
 import Foundation
 
-enum Policy {
+enum Url {
     case privacy
     case terms
+    case homepage
     
     var url: String {
         switch self {
@@ -17,6 +18,9 @@ enum Policy {
             return "https://stingy-llama-a98.notion.site/Privacy-policy-19ad45242c9b80288d33d989bc959c0d"
         case .terms:
             return "https://stingy-llama-a98.notion.site/Terms-of-service-1a2d45242c9b80fcb754f163a6ce66b6?pvs=73"
+        case .homepage:
+            return "https://stingy-llama-a98.notion.site/Just-Countdown-1a5d45242c9b8003a4f9fce20acdc442"
         }
     }
 }
+
